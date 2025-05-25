8af804156bb99cb006d4d4272ad3013e2ac52ebd
marcomaddo <profmaddo@gmail.com>
2025-05-25 15:59:43 -0400

    Inicial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SRE Challenge</h1>
+        <p>Welcome to our CI/CD pipeline demo!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
